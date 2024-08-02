86a176a82567d419260bd89454cea8ac0b4ed4f2
291e <156879219+291e@users.noreply.github.com>
2024-08-02 10:22:14 +0900

    setting

---
+++ index.js
@@ -0,0 +1,63 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+
+  type Movie {
+    title: String
+    year: Int
+  }
+    
+
+  type Query {
+    movies: [Movie]
+    movie: Movie
+  }
+
+  type Mutation {
+    createMovie(title:String!): Boolean
+    deleteMovie(title:String!): Boolean
+  }
+`;
+
+const movies = [
+  {
+    title: "The Amazing",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    movies: () => movies,
+    movie: () => ({ title: "Hello", year: 2021 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
